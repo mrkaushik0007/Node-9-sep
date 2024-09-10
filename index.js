6c15297ae6b21c865c2a7e88b8cf6375561da208
ansh kaushik <dkk124421@gmail.com>
2024-09-10 14:58:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const user =require('./routes/user')
+
+app.use(user)
+
+
+
+app.listen(3000, (err)=> {
+    if(err) {
+        console.log("error")
+    } else {
+        console.log("server is running on 3000")
+    }
+})
